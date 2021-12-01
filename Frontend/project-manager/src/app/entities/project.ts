ad383bb609f4a53ea20beb80c4576aa8aee03a55
LukSiman <simanavicius.lukas@gmail.com>
2021-11-30 18:27:13 -0800

    a

--- Frontend/project-manager/src/app/entities/project.ts
+++ Frontend/project-manager/src/app/entities/project.ts
@@ -1,6 +1,9 @@
+import { ProjectImages } from "./project-images";
+
 export class Project {
     name: string;
     startDate: Date;
     endDate: Date;
     description: string;
+    images: ProjectImages[] = [];
 }
