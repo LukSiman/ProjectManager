60e9d99b893cd60fc505208c7311c10396a07ccf
LukSiman <simanavicius.lukas@gmail.com>
2021-12-04 20:47:22 -0800

    Adding pagination support

--- Backend/project_manager/src/main/java/com/lukas/project_manager/dao/ProjectRepository.java
+++ Backend/project_manager/src/main/java/com/lukas/project_manager/dao/ProjectRepository.java
@@ -1,9 +1,16 @@
 package com.lukas.project_manager.dao;
 
 import com.lukas.project_manager.entities.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+    Page<Project> findBy(Pageable pageable);
+
+    Page<Project> findByName(@Param("name") String name, Pageable pageable);
 }
