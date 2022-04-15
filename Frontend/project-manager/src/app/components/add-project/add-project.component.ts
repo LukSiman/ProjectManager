d1419e4c3f59b76321fd051eaefed79d737b1019
LukSiman <simanavicius.lukas@gmail.com>
2022-04-15 16:58:04 -0700

    adding task and image handling to new projects

--- Frontend/project-manager/src/app/components/add-project/add-project.component.ts
+++ Frontend/project-manager/src/app/components/add-project/add-project.component.ts
@@ -1,7 +1,8 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faArrowsAlt, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Project } from 'src/app/entities/project';
-import { ProjectImages } from 'src/app/entities/project-images';
 import { ProjectService } from 'src/app/services/project.service';
 import { CustomValidators } from 'src/app/validators/custom-validators';
 
@@ -14,7 +15,10 @@ export class AddProjectComponent implements OnInit {
 
   projectFormGroup: FormGroup;
   errorMessage: string;
+  closeBtn = faTimes;
+  dragHandle = faArrowsAlt;
   statuses: string[] = ['New idea', 'In progress', 'On hold', 'Dropped', 'Completed'];
+  taskStatuses: string[] = ['Not started', 'In progress', 'On hold', 'Completed'];
 
   private selectedFile: File;
   private newFileName: string;
@@ -23,13 +27,14 @@ export class AddProjectComponent implements OnInit {
 
   ngOnInit(): void {
     this.projectFormGroup = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(4), CustomValidators.notOnlyWhitespace]],
+      name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50), CustomValidators.notOnlyWhitespace]],
       description: [''],
       startDate: ['', Validators.required],
       endDate: [''],
       status: [this.statuses[0]],
-      images: ['', CustomValidators.correctFileType],
-      tasks: ['']
+      images: this.formBuilder.array([]),
+      tasks: this.formBuilder.array([]),
+      fileUpload: ['']
     }, { validators: CustomValidators.endDateAfterStartDate });
   }
 
@@ -38,8 +43,9 @@ export class AddProjectComponent implements OnInit {
   get startDate() { return this.projectFormGroup.get('startDate'); }
   get endDate() { return this.projectFormGroup.get('endDate'); }
   get status() { return this.projectFormGroup.get('status'); }
-  get images() { return this.projectFormGroup.get('images'); }
-  get tasks() { return this.projectFormGroup.get('tasks'); }
+  get images() { return this.projectFormGroup.get('images') as FormArray; }
+  get tasks() { return this.projectFormGroup.get('tasks') as FormArray; }
+  get fileUpload() { return this.projectFormGroup.get('fileUpload'); }
 
   // informs user of required fields and initiates project adding to DB 
   checkForm(): void {
@@ -54,9 +60,15 @@ export class AddProjectComponent implements OnInit {
       return;
     }
 
+    // changes taskOrder values 
+    for (let i = 0; i < this.tasks.controls.length; i++) {
+      this.tasks.controls[i].get('taskOrder')?.setValue(i + 1);
+    }
+
     this.projectCreationController();
   }
 
+  // Controller for handling project creation
   private async projectCreationController(): Promise<void> {
     if (this.selectedFile != undefined || this.selectedFile != null) {
       try {
@@ -75,30 +87,16 @@ export class AddProjectComponent implements OnInit {
       await this.addNewProject();
     }
 
+    // reset the form
     this.formCleaner();
   }
 
-  // resets fields of the form
-  private formCleaner(): void {
-    // resets the form
-    this.projectFormGroup.reset();
-
-    // resets the selected file
-    this.selectedFile = undefined as unknown as File;
-
-    this.status?.setValue(this.statuses[0]);
-  }
-
-    // manual form reset
-   formCleanerManual(): void {
-      this.formCleaner();
-      this.errorMessage = '';
-    }
-
   // add a new project
   private addNewProject(): Promise<Object> {
-    // create a project image object 
-    let newProjectImage: ProjectImages = this.handleProjectImages();
+    // checks if a file was selected and set as the image
+    if (this.selectedFile != undefined || this.selectedFile != null) {
+      this.handleProjectImages();
+    }
 
     // creates a new project to send to the server
     const newProject: Project = {
@@ -109,8 +107,8 @@ export class AddProjectComponent implements OnInit {
       endDate: this.endDate?.value,
       length: 0,
       status: this.status?.value,
-      images: [newProjectImage],
-      tasks: []
+      tasks: this.tasks?.value,
+      images: this.images?.value
     };
 
     // call service to add the project
@@ -123,23 +121,57 @@ export class AddProjectComponent implements OnInit {
     });
   }
 
-  // create and return a new ProjectImage object
-  private handleProjectImages(): ProjectImages {
-    // create empty ProjectImages object
-    let newProjectImages: ProjectImages = {
-      imageId: 0,
-      imageUrl: ''
-    }
+  // adds a new task to the task list
+  addNewTask(): void {
+    let projectTask: FormGroup = this.formBuilder.group({
+      taskOrder: [this.tasks.length + 1],
+      taskDescription: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(4), CustomValidators.notOnlyWhitespace]],
+      taskStatus: [this.taskStatuses[0], Validators.required]
+    });
 
-    // checks if a file was selected and set as the image
-    if (this.selectedFile != undefined || this.selectedFile != null) {
-      newProjectImages = {
-        imageId: 0,
-        imageUrl: `assets/images/projects/${this.newFileName}`
-      }
-    }
+    this.tasks.push(projectTask);
+  }
 
-    return newProjectImages;
+  // removes the task from the task list
+  removeTask(index: number): void {
+    this.tasks.removeAt(index);
+  }
+
+  // resets fields of the form
+  private formCleaner(): void { //TODO:
+    // resets the form
+    this.projectFormGroup.reset();
+
+    // resets tasks FormArray
+    this.tasks.clear();
+
+    // resets the file upload field
+    this.fileUpload?.reset();
+
+    // resets the selected file
+    this.selectedFile = undefined as unknown as File;
+
+    // reset the file name
+    this.newFileName = undefined as unknown as string;
+
+    // reset the error message
+    this.errorMessage = '';
+
+    this.status?.setValue(this.statuses[0]);
+  }
+
+  // manual form reset
+  formCleanerManual(): void {
+    this.formCleaner();
+  }
+
+  // add a new image to the FormArray
+  private handleProjectImages(): void {
+    let projectImage = this.formBuilder.group({
+      imageUrl: [`assets/images/projects/${this.newFileName}`]
+    });
+
+    this.images.push(projectImage);
   }
 
   // selects the file
@@ -149,9 +181,13 @@ export class AddProjectComponent implements OnInit {
 
   // handles image uploading
   private uploadImage(): Promise<Object> {
-    const formData = new FormData();
+    const formData: FormData = new FormData();
     this.newFileName = this.fileNameGenerator(this.selectedFile.name);
     formData.append('file', this.selectedFile, this.newFileName);
+
+    // the current count of images in the array to check against the limit
+    let count: string = `${this.images.length}`;
+    formData.append('count', count);
 
     return new Promise((resolve, reject) => {
       this.projectService.uploadImage(formData).subscribe(response => {
@@ -164,9 +200,22 @@ export class AddProjectComponent implements OnInit {
 
   // Add a timestamp to the filename
   private fileNameGenerator(fileName: string): string {
-    const date = new Date();
-    const timeStamp = date.getTime();
-    fileName = fileName.substring(0, fileName.lastIndexOf('.'));
-    return `${fileName}${timeStamp}.png`;
+    const date: Date = new Date();
+    const timeStamp: number = date.getTime();
+
+    let extension: string = '';
+
+    if (fileName.includes('.')) {
+      let extensionIndex: number = fileName.lastIndexOf('.');
+      extension = fileName.substring(extensionIndex);
+      fileName = fileName.substring(0, extensionIndex);
+    }
+
+    return `${fileName}${timeStamp}${extension}`;
+  }
+
+  // allows dragging the tasks
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.tasks.controls, event.previousIndex, event.currentIndex);
   }
 }
