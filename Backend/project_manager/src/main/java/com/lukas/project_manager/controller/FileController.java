338f7fdd74e18fb2205314f962c32baac6d60cae
LukSiman <simanavicius.lukas@gmail.com>
2022-04-27 22:47:29 -0700

    Adding to cors

--- Backend/project_manager/src/main/java/com/lukas/project_manager/controller/FileController.java
+++ Backend/project_manager/src/main/java/com/lukas/project_manager/controller/FileController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.ResponseEntity.status;
 
+@CrossOrigin
 @RestController
 @RequestMapping("projects")
 public class FileController {
--- Backend/project_manager/src/main/java/com/lukas/project_manager/controller/ImageController.java
+++ Backend/project_manager/src/main/java/com/lukas/project_manager/controller/ImageController.java
@@ -4,6 +4,7 @@ import com.lukas.project_manager.service.ImageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/images")
 public class ImageController {
