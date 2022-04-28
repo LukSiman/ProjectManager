a811fa76d7da465cd4df539d8c8379245923142c
LukSiman <simanavicius.lukas@gmail.com>
2022-04-27 22:38:33 -0700

    Adding jwt tokens

---
+++ Backend/project_manager/src/main/java/com/lukas/project_manager/controller/JwtAuthenticationController.java
@@ -0,0 +1,66 @@
+package com.lukas.project_manager.controller;
+
+import com.lukas.project_manager.config.JwtTokenUtil;
+import com.lukas.project_manager.dto.UserDTO;
+import com.lukas.project_manager.entities.JwtRequest;
+import com.lukas.project_manager.entities.JwtResponse;
+import com.lukas.project_manager.exceptions.UserAlreadyExistsException;
+import com.lukas.project_manager.service.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
+        return ResponseEntity.ok(userDetailsService.save(user));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED");
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+//            throw new BadCredentialsException("INVALID_CREDENTIALS");
+            // TODO: why exception not thrown?
+//            throw new UserAlreadyExistsException();
+        }
+    }
+}
---
+++ Backend/project_manager/src/main/java/com/lukas/project_manager/service/JwtUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.lukas.project_manager.service;
+
+import com.lukas.project_manager.dao.UserRepository;
+import com.lukas.project_manager.dto.UserDTO;
+import com.lukas.project_manager.entities.User;
+import com.lukas.project_manager.exceptions.UserAlreadyExistsException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+    public User save(UserDTO user) {
+        if(userRepository.existsUserByUsername(user.getUsername())){
+            throw new UserAlreadyExistsException();
+        }
+
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+    }
+}
